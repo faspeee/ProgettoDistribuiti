e39946b98c67a5c822e6cf941e26d21172419db3
faspeeencina <fabian.aspeeencina@studio.unibo.it>
2020-11-16 22:20:40 +0100

    test

---
+++ Zyzzyva/src/Main/Actor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text; 
+using Akka.Actor; 
+namespace Zyzzyva.src.Main
+{
+    class Actor : ReceiveActor
+    { 
+        private int fibonacci(int x){
+            static int fibHelper(int xx, int prev = 0, int next = 1) => xx switch
+            {
+                0 => prev,
+                1 => next,
+                _ => fibHelper(xx - 1, next, next + prev)
+            };
+
+            return fibHelper(x);
+        }
+        Func<int, int> multiplyByFive = x =>
+        {
+            static int fibHelper(int xx, int prev = 0, int next = 1) => xx switch
+            {
+                0 => prev,
+                1 => next,
+                _ => fibHelper(xx - 1, next, next + prev)
+            };
+
+            return fibHelper(x);
+        };
+    }
+}
---
+++ Zyzzyva/src/Main/Option/Core/Option.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zyzzyva.src.Main.Option.Core
+{
+    public interface IOption<T>
+    {
+        bool IsEmpty<A>(IOption<A> opt);
+        B getOrElse<A, B>(IOption<A> opt, B orElse) where B : A;
+        IOption<B> flatMap<A, B>(Func<A,IOption<B>> f);
+    }
+    public class Some<T> : Impl<T>
+    { 
+        public T _a { get; }
+        public Some(T a) => _a = a;
+    }
+    public class None<T> : Impl<T>
+    { }
+    public abstract class Impl<T> : IOption<T>
+    {
+        public IOption<B> flatMap<A, B>(Func<A, IOption<B>> f) => this switch
+        {
+            new Some(a)=>f(a),
+            _ => new None();
+        }; 
+
+        public B getOrElse<A, B>(IOption<A> opt, B orElse) where B : A
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty<A>(IOption<A> opt)=> opt switch
+        {
+            new None() => true,
+            _ => false
+        };
+    }
+}
+     
