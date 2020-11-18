8875f5c7c57e5a5ce37013e6187f5a32f67bba18
giovanni-mormone <giovanni.mormone@studio.unibo.it>
2020-11-18 15:27:06 +0100

    rest test

---
+++ ZyzzyvaREST/Service1.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ZyzzyvaREST
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di classe "Service1" nel codice, nel file svc e nel file di configurazione contemporaneamente.
+    // NOTA: per avviare il client di prova WCF per testare il servizio, selezionare Service1.svc o Service1.svc.cs in Esplora soluzioni e avviare il debug.
+    public class Service1 : IService1
+    {
+        public List<string> GetClusterMembers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetFibonacci(int number)
+        {
+            return number +1;
+        }
+    }
+}
