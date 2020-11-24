95bd02ff8aa30b34631e10c99a605cd239cf2f48
faspeeencina <faspeeencina@gmail.com>
2020-11-24 02:34:05 +0100

    docker network

--- Zyzzyva/Program.cs
+++ Zyzzyva/Program.cs
@@ -7,35 +7,32 @@ using Zyzzyva.Akka.Matematica;
 using System.IO;
 using Zyzzyva.Akka.Membri;
 using Zyzzyva.Akka.Database;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Diagnostics;
-
+using Akka.Bootstrap.Docker;
 namespace Zyzzyva
-{ 
+{
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(string.Join(",", args));
-             StartUp(args.Length == 1 ? args[0] : "2554");
+           
+             StartUp(args.Length == 1 ? args[0] : "2553");
         } 
         public static void StartUp(string port)
         {
             
             var hocon = File.ReadAllText(ConfigurationManager.AppSettings["configpath"]);
+            Console.WriteLine(hocon + Environment.GetEnvironmentVariable("CLUSTER_PORT"));
             var section = ConfigurationFactory.ParseString(hocon);
-            // var section = (AkkaConfigurationSection)ConfigurationManager.GetSection("akka");
-
+            // var section = (AkkaConfigurationSection)ConfigurationManager.GetSection("akka"); 
             //Override the configuration of the port
+
             var config =
                 ConfigurationFactory.ParseString("akka.remote.dot-netty.tcp.port=" + port)
-                    .WithFallback(section);
-
+                    .WithFallback(section);//.BootstrapFromDocker();
+            
             //create an Akka system
             var system = ActorSystem.Create("cluster-playground", config);
-
+           
             //create an actor that handles cluster domain events
             var matematicaManager = system.ActorOf(MatematicaManagerActor.MyProps(port), "matematica_manager");
             var membriManager = system.ActorOf(MembriManagerActor.MyProps(port), "member_manager");
